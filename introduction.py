c488baecde1633e6e7fc1148789f8da734a078bc
Sagarika Chakraborty <sagarikachakrabortyrai2027@gmail.com>
2026-09-25 16:49:41 +0530

    Strings and Personal Introduction Program

---
+++ introduction.py
@@ -0,0 +1,40 @@
+name = "Sagarika"
+print(name)
+
+profession= 'Technical lead engineer'
+print(profession)
+
+Hobby= "Author"
+print(Hobby)
+
+Message = """ I want to be an AI engineer ."""
+print(Message)
+
+#Concatenation of strings
+First_name = "Sagarika" 
+Last_name = "Chakraborty"
+Full_name = First_name + " " + Last_name
+print(Full_name)
+
+#fstrings
+
+name="Rahul"
+print(f"My brother's name is {name}")
+
+
+#Lowercase()method
+name = "Sagarika"
+pin = 560035
+print(name.lower())
+print(name.upper())
+#Uppercase()method
+
+print(len(name))
+
+#Datatype
+
+print(type(name))
+print(type(pin))
+
+Author_name = False
+print(type(Author_name))
